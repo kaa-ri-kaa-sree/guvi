882e88107f83ed9b3de9072e8573f3aebdbf90c7
kaa-ri-kaa-sree <53809309+kaa-ri-kaa-sree@users.noreply.github.com>
2019-08-06 14:35:00 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+n=str(input())
+list=['a','e','i','o','u']
+symbol=['@','!','$','&']
+if n in list:
+    print('Vowel')
+elif n in symbol:
+    print('Invalid')
+elif n is not list:
+    print('Consonant')
