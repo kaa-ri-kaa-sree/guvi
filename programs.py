252c07f1516eb2a5e3f42109439e4d25eb940d6a
kaa-ri-kaa-sree <53809309+kaa-ri-kaa-sree@users.noreply.github.com>
2019-08-06 12:06:34 +0530

    Create programs.py

---
+++ programs.py
@@ -0,0 +1,15 @@
+a=int(input("enter the value :"))
+b=int(input("enter the value :"))
+c=int(input("enter the value :"))
+if (c==1):
+ d=a+b
+ print(d)
+elif (c==2):
+  d=a-b
+  print(d)
+elif (c==3):
+    d=a*b
+    print(d)
+elif (c==4):
+   d=a/b
+   print(d)
